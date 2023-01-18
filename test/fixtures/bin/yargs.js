74e6d4d062b63e0ddf47fba77c82b2f8018a49b2
ponysay <122148421+ponysay@users.noreply.github.com>
2023-01-18 09:14:17 +0000

    Updated Telugu translation

--- test/fixtures/bin/yargs.js
+++ test/fixtures/bin/yargs.js
@@ -1,4 +1,16 @@
 #! /usr/bin/env node
 
 var program = require('yargs')
-var surge = require('../../../
+var surge = require('../../../')({
+  name: 'yargs'
+})
+
+var hooks = {}
+
+program
+  .command('login', 'Login to your account', surge.login(hooks))
+  .usage('$0 <command>')
+  .argv
+
+program
+  .command('whoami', 'Check who you are logged in as.', surge.whoa

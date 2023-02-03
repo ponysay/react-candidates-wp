5d8c7a2cf63214c24745fcf440bc115dd86a6530
ponysay <122148421+ponysay@users.noreply.github.com>
2023-02-03 20:38:17 +0000

    Release v1.0.1.

--- test/publish.js
+++ test/publish.js
@@ -1,2 +1,9 @@
 var nixt = require('nixt')
-var should = re
+var should = require('should')
+var pkg = require('../package.json')
+
+var endpoint = typeof process.env.ENDPOINT !== 'undefined' ? ' -e ' + process.env.ENDPOINT + ' ' : ' '
+var surge = 'node ' + pkg.bin + endpoint
+var opts = {
+  colors: false,
+  new

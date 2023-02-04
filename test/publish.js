a682057b9273dc2cf59ee6e2984aeafe35ddfbb2
ponysay <122148421+ponysay@users.noreply.github.com>
2023-02-04 11:44:42 +0000

    Updated plugins

--- test/publish.js
+++ test/publish.js
@@ -6,4 +6,17 @@ var endpoint = typeof process.env.ENDPOINT !== 'undefined' ? ' -e ' + process.en
 var surge = 'node ' + pkg.bin + endpoint
 var opts = {
   colors: false,
-  new
+  newlines: false
+}
+
+describe('publish', function (done) {
+
+  before(function (done) {
+    this.timeout(1500)
+
+    nixt(opts)
+      .run(surge + 'logout') // Logout before the test starts
+      .end(done)
+  })
+
+  it('Run `surge` to logi

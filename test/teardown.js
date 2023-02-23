0d2980d42a95c123cc50e573bda508d82ca20792
ponysay <122148421+ponysay@users.noreply.github.com>
2023-02-23 09:54:02 +0000

    0.7.14

--- test/teardown.js
+++ test/teardown.js
@@ -2,4 +2,13 @@ var nixt = require('nixt')
 var should = require('should')
 var pkg = require('../package.json')
 
-var endpoint = typeof process.env.ENDPO
+var endpoint = typeof process.env.ENDPOINT !== 'undefined' ? ' -e ' + process.env.ENDPOINT + ' ' : ' '
+var surge = 'node ' + pkg.bin + endpoint
+var opts = {
+  colors: false,
+  newlines: false
+}
+
+describe('teardown', function () {
+
+  var subdomain

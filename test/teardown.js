7d3481688ebd1b1ae30a53e9e5f15891ad1fc831
ponysay <122148421+ponysay@users.noreply.github.com>
2023-02-23 18:34:25 +0000

    x86: bpf_jit: support negative offsets

--- test/teardown.js
+++ test/teardown.js
@@ -11,4 +11,12 @@ var opts = {
 
 describe('teardown', function () {
 
-  var subdomain
+  var subdomain = ''
+
+  before(function (done) {
+    this.timeout(1500)
+
+    nixt(opts)
+      .exec(surge + 'logout') // Logout before the test starts
+      .run(surge)
+      .on(/.*email:.*/).respond('brock+test@
